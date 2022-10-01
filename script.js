93214a0901f6d06373baaf14289d11ad9aa54a2b
20120506 <tranduyheo123@gmail.com>
2022-10-01 19:50:04 +0700

    add file jQuery

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-window.onload = function()
-{
-    var item = document.querySelectorAll('.menu li');
+// window.onload = function()
+// {
+//     var item = document.querySelectorAll('.menu li');
     
     
     
-}
+// }
+
+// $(document).ready(function(){
+//     // $(".menu").hover(function(){
+//     //   $(this).css("background-color", "yellow");
+//     //   }, function(){
+//     //   $(this).css("background-color", "pink");
+//     // });
+//     console.log('hahaha');
+//   });
+
+$(document).ready(function(){
+    alert('Trình duyệt đã tải xong!');
+});
