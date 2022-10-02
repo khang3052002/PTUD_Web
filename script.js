3002b38e99ab41fb5e6ba1f4509e321625ff343d
20120506 <tranduyheo123@gmail.com>
2022-10-02 12:20:40 +0700

    code function xử  lí khi nhấn button Xóa hết

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(function () {
                 menu_footer.eq(i).css("background-color", "orange");
                 $(this).css("color", "#fcc782");
                 temp_footer = menu_footer.eq(i);
-                console.log(i);
+                // console.log(i);
                 break;
             }
         }
@@ -80,22 +80,108 @@ $(function () {
         var regex_maSo =/^1[7-9]{1}|2[0-2]{1}[0-9]{6}$/;
         var flag_maSo = regex_maSo.test(maSo);
         console.log(flag_maSo);
+
+        // xu li ho ten 
+        var hoTen = document.querySelector('#hoten').value;
+        let regex_hoTen = /^([A-VXYỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ]{1}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ]+)((\s{1}[A-VXYỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ]{1}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ]+){1,})$/;
+        var flag_hoTen = regex_hoTen.test(hoTen);
+        console.log(flag_hoTen);
+        // xử lí địa chỉ
+        var diaChi = document.querySelector('#diachi').value;
+        let regex_diaChi = /^([A-VXYỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ0-9]{1}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ0-9]+)((\s{1}[A-VXYỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ0-9]{1}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ0-9]+){1,})$/;
+        var flag_diaChi = regex_hoTen.test(diaChi);
+        console.log(flag_diaChi);
         //xu li dien thoai
         var dienThoai = document.querySelector('#dienthoai').value;
         var regex_dt =/^[0]{1}[0-9]{9}$/;
         var flag_dt = regex_dt.test(dienThoai);
-
         console.log(flag_dt);
+        // xu li gioi tinh
+        var gioiTinh;
+        if($('#nam').is(':checked'))
+        {
+            gioiTinh = 'Nam';
+        }
+        else if($('#nu').is(':checked'))
+        {
+            gioiTinh = 'Nữ';
+        }
+        else{
+            alert('sex');
+        }
+        // console.log(gioiTinh);
+        // xu li ngay sinh
+        var ngaySinh = document.querySelector('#ngaysinh').value;
+        // var tempDate = new Date(ngaySinh);
+        // var formattedDate = [tempDate.getMonth() + 1, tempDate.getDate(), tempDate.getFullYear()].join('/');
+        // console.log(formattedDate) ;
+
+        var day = new Date(ngaySinh);
+        var dd = String(day.getDate()).padStart(2, '0');
+        var mm = String(day.getMonth() + 1).padStart(2, '0'); 
+        var yyyy = day.getFullYear();
+        day = mm + '/' + dd + '/' + yyyy; 
+        console.log(day);
 
         // xu li email
         var email = document.querySelector('#email').value;
         var regex_email = /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,}){1,3}$/;
         var flag_email = regex_email.test(email);
-
         console.log(flag_email);
 
+        var list = [
+        {flag: flag_maSo, id: '#maso' },
+        {flag: flag_hoTen, id: '#hoten'},
+        {flag: flag_diaChi, id: '#diachi'},
+        {flag: flag_dt, id: '#dienthoai'},
+        {flag: flag_email, id:'#email'}
+    ]
+    console.log(list);
+        var temp_id_old;
+        for(let i in list)
+        {
+            if(list[i].flag == false)
+            {
+                $(list[i].id).css("border","2px solid red");
+                //$(list[i].id).after('<i class="fa-solid fa-xmark" id="xmark" ></i>');
+                // $(list[i].id).addClass('fa fa-arrow-right');
+                // $('row-input').css("position","absolute");
+                temp_id_old = $(list[i].id).parent().attr('id');
+                //temp_id_old = '#' + temp_id_old + ' i';
+                // console.log($(temp_id_old));
+                temp_id_old = '#' + temp_id_old;
+                $(temp_id_old +' #xmark').css("display","inline");
+                $(temp_id_old +' #tick_check').css("display","none");
+                
+            }
+            else{
+                console.log('hahahaha');
+                temp_id_old = $(list[i].id).parent().attr('id');
+                temp_id_old = '#' + temp_id_old;
+                $(temp_id_old +' #tick_check').css("display","inline");
+                $(temp_id_old +' #xmark').css("display","none");
+                
+                //$(temp_id_old).remove();
+                // $('i').removeClass("fa-solid fa-xmark");
+                // $('i').remove();
+                //console.log($(list[i].id).parent().attr('id'));
+                // $(temp_id_old)
+                $(list[i].id).css("border","1px solid #333");
+                //$(list[i].id).after('<i class="fa-solid fa-check" id="tick_check"></i>');
+
+            }
+            
+        }
+        //console.log(list);
+        
 
         event.preventDefault();
+    });
+
+    $('#xoa').click(function()
+    {
+        $('.row-input i').css("display","none");
+        $('form').trigger("reset");
     });
 });
 
@@ -229,4 +315,8 @@ function checkInput()
     var maSo = document.querySelector('#maso')
     var str_maso = maSo.value;
     console.log(str_maso);
+}
+function resetForm(e)
+{
+    $('form').trigger("reset");
 }
